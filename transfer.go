7e6e49a913af5cbd93fc0325f5b5b304c501a686
Ivan Stasiuk <brokeyourbike@gmail.com>
2023-12-02 22:27:58 +0000

    feat: more transfer methods

--- transfer.go
+++ transfer.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"strconv"
 )
 
 type NIPBanksResponse struct {
@@ -76,13 +75,129 @@ type TransactionStatusResponse struct {
 	Date            Time   `json:"transactionDateTime"`
 }
 
-func (r TransactionStatusResponse) ParseAmount() float64 {
-	v, _ := strconv.ParseFloat(r.Amount, 64)
-	return v
-}
-
 func (c *transferClient) GetTransactionStatus(ctx context.Context, reference string) (data TransactionStatusResponse, err error) {
 	req, err := c.newRequest(ctx, http.MethodPost, "/api/GetProvidusTransactionStatus", &getTransactionStatusPayload{Reference: reference})
+	if err != nil {
+		return data, fmt.Errorf("failed to create request: %w", err)
+	}
+
+	req.DecodeTo(&data)
+	return data, c.do(ctx, req)
+}
+
+func (c *transferClient) GetNIPTransactionStatus(ctx context.Context, reference string) (data TransactionStatusResponse, err error) {
+	req, err := c.newRequest(ctx, http.MethodPost, "/api/GetNIPTransactionStatus", &getTransactionStatusPayload{Reference: reference})
+	if err != nil {
+		return data, fmt.Errorf("failed to create request: %w", err)
+	}
+
+	req.DecodeTo(&data)
+	return data, c.do(ctx, req)
+}
+
+type getAccountPayload struct {
+	authPayload
+	AccountNumber string `json:"accountNumber"`
+}
+
+type AccountResponse struct {
+	ResponseCode    string `json:"responseCode"`
+	ResponseMessage string `json:"responseMessage"`
+}
+
+func (c *transferClient) GetAccount(ctx context.Context, accountNumber string) (data AccountResponse, err error) {
+	req, err := c.newRequest(ctx, http.MethodPost, "/api/GetProvidusAccount", &getAccountPayload{AccountNumber: accountNumber})
+	if err != nil {
+		return data, fmt.Errorf("failed to create request: %w", err)
+	}
+
+	req.DecodeTo(&data)
+	return data, c.do(ctx, req)
+}
+
+type getNIPAccountPayload struct {
+	authPayload
+	AccountNumber string `json:"accountNumber"`
+	BankCode      string `json:"beneficiaryBank"`
+}
+
+type NIPAccountResponse struct {
+	ResponseCode         string `json:"responseCode"`
+	ResponseMessage      string `json:"responseMessage"`
+	AccountNumber        string `json:"accountNumber"`
+	AccountName          string `json:"accountName"`
+	BankCode             string `json:"bankCode"`
+	BVN                  string `json:"bvn"`
+	TransactionReference string `json:"transactionReference"`
+}
+
+func (c *transferClient) GetNIPAccount(ctx context.Context, bankCode, accountNumber string) (data NIPAccountResponse, err error) {
+	req, err := c.newRequest(ctx, http.MethodPost, "/api/GetNIPAccount", &getNIPAccountPayload{BankCode: bankCode, AccountNumber: accountNumber})
+	if err != nil {
+		return data, fmt.Errorf("failed to create request: %w", err)
+	}
+
+	req.DecodeTo(&data)
+	return data, c.do(ctx, req)
+}
+
+type fundTransferPayload struct {
+	authPayload
+	FundTransferPayload
+}
+
+type FundTransferPayload struct {
+	CreditAccount string `json:"creditAccount"`
+	DebitAccount  string `json:"debitAccount"`
+	Currency      string `json:"currencyCode"`
+	Amount        string `json:"transactionAmount"`
+	Reference     string `json:"transactionReference"`
+	Narration     string `json:"narration"`
+}
+
+type FundTransferResponse struct {
+	ResponseCode    string `json:"responseCode"`
+	ResponseMessage string `json:"responseMessage"`
+	Currency        string `json:"currency"`
+	Amount          string `json:"amount"`
+	Reference       string `json:"transactionReference"`
+}
+
+func (c *transferClient) FundTransfer(ctx context.Context, payload FundTransferPayload) (data FundTransferResponse, err error) {
+	req, err := c.newRequest(ctx, http.MethodPost, "/api/ProvidusFundTransfer", &fundTransferPayload{FundTransferPayload: payload})
+	if err != nil {
+		return data, fmt.Errorf("failed to create request: %w", err)
+	}
+
+	req.DecodeTo(&data)
+	return data, c.do(ctx, req)
+}
+
+type nipFundTransferPayload struct {
+	authPayload
+	NIPFundTransferPayload
+}
+
+type NIPFundTransferPayload struct {
+	SourceAccountName        string `json:"sourceAccountName"`
+	BeneficiaryAccountName   string `json:"beneficiaryAccountName"`
+	BeneficiaryAccountNumber string `json:"beneficiaryAccountNumber"`
+	BeneficiaryBank          string `json:"beneficiaryBank"`
+	Currency                 string `json:"currencyCode"`
+	Amount                   string `json:"transactionAmount"`
+	Reference                string `json:"transactionReference"`
+	Narration                string `json:"narration"`
+}
+
+type NIPFundTransferResponse struct {
+	ResponseCode    string `json:"responseCode"`
+	ResponseMessage string `json:"responseMessage"`
+	Reference       string `json:"transactionReference"`
+	SessionID       string `json:"sessionId"`
+}
+
+func (c *transferClient) NIPFundTransfer(ctx context.Context, payload NIPFundTransferPayload) (data NIPFundTransferResponse, err error) {
+	req, err := c.newRequest(ctx, http.MethodPost, "/api/NIPFundTransfer", &nipFundTransferPayload{NIPFundTransferPayload: payload})
 	if err != nil {
 		return data, fmt.Errorf("failed to create request: %w", err)
 	}
